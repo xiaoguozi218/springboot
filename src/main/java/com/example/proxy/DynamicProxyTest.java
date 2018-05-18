6ba9cef35b4ec56a24ac40a15c3b66b0f8d7c743
xiaoguozi <527768758@qq.com>
2018-05-18 17:04:45 +0800

    proxy

--- src/main/java/com/example/proxy/DynamicProxyTest.java
+++ src/main/java/com/example/proxy/DynamicProxyTest.java
@@ -16,7 +16,17 @@ import java.lang.reflect.Proxy;
  *
  *  动态代理优势：
  * 不在于少写代码，而在于，
- * 在原始类和接口还未知的时候 ，确定代理类的代理行为，也就是让代理类与原始类脱离了直接联系
+ * 在原始类和接口还未知的时候 ，确定代理类的代理行为，也就是让代理类与原始类脱离了直接联系.
+ *
+ *
+ *
+ * 动态代理有两个主要应用：
+ * 1、第一是如果代码中多处会用到相同的代码，如安全性检查，权限的验证（AOP），在很多业务模块都会用到，一般都是放在我们业务代码的头部或者是尾部，那么如果不用动态代理的话，这些相同的代码就会分散在你程序的各个部分，首先代码复用率太差，其次是可维护性不高。
+ * 2、第二个主要应用，是类似拦截器的一种应用，拦截器就是在你业务代码前，加上检查性代码，如果通过则执行你的业务代码，如果不通过则不执行你的业务代码。
+ *
+ *
+ * 注意：
+ * 1、 java jdk动态代理其实是字节码生成技术与反射机制的一个结合
  *
  */
 public class DynamicProxyTest {
@@ -33,17 +43,22 @@ public class DynamicProxyTest {
     }
 
 
-    static class DynamicProxy implements InvocationHandler {
+    static class DynamicProxy implements InvocationHandler {    //这部分代码需要我们自己实现InvocationHandler接口
 
-        Object oriObj;
+        Object oriObj;  // 被代理类的实例
 
         Object bind(Object oriObj) {
             this.oriObj = oriObj;
             return Proxy.newProxyInstance(oriObj.getClass().getClassLoader(),oriObj.getClass().getInterfaces(),this);
+            //Proxy的静态方法，newProxyInstance（），这个方法有三个参数，
+            // 第一个参数是被代理类的类加载器（classLoader），
+            // 第二个参数是被代理类的所有接口，
+            // 第三个参数是我们上面实现类（InvocationHandler）的实例，
+            // 那么这个方法，返回的就是一个代理类，就是在这个方法中，动态拼接处代理类的字节码。
         }
 
         @Override
-        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {     //重写 其中的invoke方法
             System.out.println("welcome  to ");
             return method.invoke(oriObj,args);
         }
--- src/test/java/com/example/SpringbootApplicationTests.java
+++ src/test/java/com/example/SpringbootApplicationTests.java
@@ -1,11 +1,15 @@
 package com.example;
 
 import com.example.service.PayService;
+import com.example.service.RedisService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -14,10 +18,13 @@ public class SpringbootApplicationTests {
 	@Autowired
 	private PayService payService;
 
+	@Autowired
+	RedisService redisService;
+
 	@Test
 	public void payTest() throws Exception {
-		int store = payService.minGoodsnum(-1);
-		System.out.println("库存为：" + store);
+		redisService.set(UUID.randomUUID().toString(),"test",1l, TimeUnit.HOURS);
+//		System.out.println("库存为：" + store);
 	}
 
 //	@Test
