1c5c0ef1d8bf431e7ca3fa612d4ae09d216ee703
xiaoguozi <527768758@qq.com>
2018-06-02 13:15:55 +0800

    gson

--- src/main/java/com/example/service/RedisService.java
+++ src/main/java/com/example/service/RedisService.java
@@ -1,12 +1,18 @@
 package com.example.service;
 
+import com.example.utils.JSONUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -61,6 +67,90 @@ public class RedisService {
         rt.opsForValue().set(key, value, timeout, unit);
     }
 
+
+
+    public boolean set(final String key, final String value) {
+        boolean result = rt.execute(new RedisCallback<Boolean>() {
+            @Override
+            public Boolean doInRedis(RedisConnection connection) throws DataAccessException {
+                RedisSerializer<String> serializer = rt.getStringSerializer();
+                connection.set(serializer.serialize(key), serializer.serialize(value));
+                return true;
+            }
+        });
+        return result;
+    }
+
+    public String get(final String key){
+        String result = rt.execute(new RedisCallback<String>() {
+            @Override
+            public String doInRedis(RedisConnection connection) throws DataAccessException {
+                RedisSerializer<String> serializer = rt.getStringSerializer();
+                byte[] value =  connection.get(serializer.serialize(key));
+                return serializer.deserialize(value);
+            }
+        });
+        return result;
+    }
+
+
+    public boolean expire(final String key, long expire) {
+        return rt.expire(key, expire, TimeUnit.SECONDS);
+    }
+
+    public <T> boolean setList(String key, List<T> list) {
+        String value = JSONUtil.toJson(list);
+        return set(key,value);
+    }
+
+    public <T> List<T> getList(String key, Class<T> clz) {
+        String json = get(key);
+        if(json!=null){
+            List<T> list = JSONUtil.toList(json, clz);
+            return list;
+        }
+        return null;
+    }
+
+    public long lpush(final String key, Object obj) {
+        final String value = JSONUtil.toJson(obj);
+        long result = rt.execute(new RedisCallback<Long>() {
+            @Override
+            public Long doInRedis(RedisConnection connection) throws DataAccessException {
+                RedisSerializer<String> serializer = rt.getStringSerializer();
+                long count = connection.lPush(serializer.serialize(key), serializer.serialize(value));
+                return count;
+            }
+        });
+        return result;
+    }
+
+    public long rpush(final String key, Object obj) {
+        final String value = JSONUtil.toJson(obj);
+        long result = rt.execute(new RedisCallback<Long>() {
+            @Override
+            public Long doInRedis(RedisConnection connection) throws DataAccessException {
+                RedisSerializer<String> serializer = rt.getStringSerializer();
+                long count = connection.rPush(serializer.serialize(key), serializer.serialize(value));
+                return count;
+            }
+        });
+        return result;
+    }
+
+    public String lpop(final String key) {
+        String result = rt.execute(new RedisCallback<String>() {
+            @Override
+            public String doInRedis(RedisConnection connection) throws DataAccessException {
+                RedisSerializer<String> serializer = rt.getStringSerializer();
+                byte[] res =  connection.lPop(serializer.serialize(key));
+                return serializer.deserialize(res);
+            }
+        });
+        return result;
+    }
+
+
     /**
      * 用户注册去重检验 （利用redis加锁 解锁）
      * @param customerLogin
@@ -108,6 +198,8 @@ public class RedisService {
 //        }
 //
 //    }
+
+
 
 
 }
