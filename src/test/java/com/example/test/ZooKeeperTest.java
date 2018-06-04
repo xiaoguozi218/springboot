bad96db6fbb2f632a35e940496c2a4cc079eb75f
xiaoguozi <527768758@qq.com>
2018-06-04 16:21:16 +0800

    utils

---
+++ src/test/java/com/example/test/ZooKeeperTest.java
@@ -0,0 +1,40 @@
+package com.example.test;
+
+import com.example.utils.DistributedLock;
+
+/**
+ * Created by MintQ on 2018/6/4.
+ */
+public class ZooKeeperTest {
+
+    static int n = 500;
+
+    public static void secskill() {
+        System.out.println(--n);
+    }
+
+    public static void main(String[] args) {
+
+        Runnable runnable = new Runnable() {
+            public void run() {
+                DistributedLock lock = null;
+                try {
+                    lock = new DistributedLock("120.77.46.159:2181", "test1");
+                    lock.lock();
+                    secskill();
+                    System.out.println(Thread.currentThread().getName() + "正在运行");
+                } finally {
+                    if (lock != null) {
+                        lock.unlock();
+                    }
+                }
+            }
+        };
+
+        for (int i = 0; i < 10; i++) {
+            Thread t = new Thread(runnable);
+            t.start();
+        }
+    }
+
+}
