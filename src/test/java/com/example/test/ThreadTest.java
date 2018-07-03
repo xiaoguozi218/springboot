47dc795db48fa7df56b3f8d823e723483d8ca135
xiaoguozi <527768758@qq.com>
2018-07-03 10:19:39 +0800

    netty

--- src/test/java/com/example/test/ThreadTest.java
+++ src/test/java/com/example/test/ThreadTest.java
@@ -1,21 +1,48 @@
 package com.example.test;
 
+import javax.swing.table.TableCellRenderer;
+
 /**
  * Created by MintQ on 2018/6/12.
  */
-public class ThreadTest {
+public class ThreadTest implements Runnable{
 
-    public static void main(String[] args) {
-        Thread t = new Thread(){
-          public void run(){
-              pong();
-          }
-        };
-        t.run();
-        System.out.println("ping");
+    public static void main(String[] args) throws InterruptedException {
+//        Thread t = new Thread(){
+//          public void run(){
+//              pong();
+//          }
+//        };
+//        t.run();
+//        System.out.println("ping");
+        ThreadTest tt = new ThreadTest();
+        new Thread(tt).start();
+        tt.m2();
+        System.out.println("main thread b= "+b);
     }
 
     static void pong() {
         System.out.println("pong");
+    }
+
+    static int b = 100;
+    synchronized void m1() throws InterruptedException {
+        b = 1000;
+        Thread.sleep(500);
+        System.out.println("b="+b);
+    }
+
+    synchronized void m2() throws InterruptedException {
+        Thread.sleep(250);
+        b=2000;
+    }
+
+    @Override
+    public void run() {
+        try {
+            m1();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
