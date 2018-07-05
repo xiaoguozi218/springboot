aec54f3b5a01be603235aaf87ff25c362055c205
xiaoguozi <527768758@qq.com>
2018-07-05 10:02:29 +0800

    netty

--- src/main/java/com/example/utils/LambdaUtils.java
+++ src/main/java/com/example/utils/LambdaUtils.java
@@ -1,10 +1,20 @@
 package com.example.utils;
 
+
 /**
  * Created by MintQ on 2018/7/4.
+ *
+ * 从java8开始，我们就可以通过java8中的 StrameAPI 与 Lambda 表达式实现函数式编程，可以让代码变得更加高效简洁。其中我开发中用得比较多的是，Optional，SteameAPI与lambda。
+ *
+ *
+ *
  */
 public class LambdaUtils {
 
+    public static void main(String[] args) {
+
+
+    }
 
 
 }
