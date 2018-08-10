f7f7920bb9ef0370e782fbd8f824e45465f7cb56
xiaoguozi <527768758@qq.com>
2018-08-10 10:23:18 +0800

    netty

--- src/main/java/com/example/dao/DataBaseLearn.java
+++ src/main/java/com/example/dao/DataBaseLearn.java
@@ -162,6 +162,52 @@ package com.example.dao;
  *      1、用户中心业务场景
  *          - 用户中心是一个非常常见的业务，主要提供用户注册、登录、查询/修改等功能，其核心表为：
  *
+ *《MySQL 的索引是什么？怎么优化？》 -
+ *  - 背景:索引就类似大学图书馆建的书目索引,可以提高数据检索的效率,降低数据库IO成本。MySQL在300万条记录左右性能开始逐渐下降,虽然官方文档说500~800w记录,所以大数据量建立索引是非常有必要的。
+ *         MySQL提供了Explain，用于显示SQL执行的详细信息，可以进行索引的优化。
+ *  一、导致SQL执行慢的原因
+ *     1、硬件问题。- 如网络速度慢，内存不足，I/O吞吐量小，磁盘空间满了等。
+ *     2、没有索引或者索引失效。- （一般在互联网公司，DBA会在半夜把表锁了，重新建立一遍索引。- 因为当你删除某个数据的时候，索引的树结构就不完整了。）
+ *     3、数据过多（分库分表）
+ *     4、服务器调优及各个参数设置（调整my.cnf）
+ *  二、分析原因时，一定要找切入点
+ *     1.先观察，开启慢查询日志，设置相应的阈值（比如超过3秒就是慢SQL），在生产环境跑上个一天过后，看看哪些SQL比较慢。
+ *     2.Explain和慢SQL分析。比如SQL语句写的烂，索引没有或失效，关联查询太多（有时候是设计缺陷或者不得以的需求）等等。
+ *     3.Show Profile是比Explain更近一步的执行细节，可以查询到执行每一个SQL都干了什么事，这些事分别花了多少秒。
+ *  三、什么是索引？
+ *     - MySQL官方对索引的定义为：索引(Index)是帮助MySQL 高效获取数据 的 数据结构。
+ *     - 我们可以简单理解为：快速查找 排好序 的一种 数据结构。
+ *     - Mysql索引主要有两种结构：B+Tree索引和Hash索引。
+ *  四、Explain 分析 - 索引使用情况在possible_keys、key和key_len三列，接下来我们先从左到右依次讲解。
+ *     1、id ：id不同,值越大越先被执行
+ *     2、select_type：- 1、SIMPLE： 表示此查询不包含 UNION 查询或子查询
+ *                      2、PRIMARY： 表示此查询是最外层的查询
+ *                      3、SUBQUERY： 子查询中的第一个 SELECT
+ *                      4、UNION： 表示此查询是 UNION 的第二或随后的查询
+ *     3、table：table表示查询涉及的表或衍生的表
+ *     4、type：- type 字段比较重要，它提供了判断查询是否高效的重要依据依据。 通过 type 字段，我们判断此次查询是 全表扫描 还是 索引扫描等。
+ *       -type 常用的取值有:1、system: 表中只有一条数据， 这个类型是特殊的 const 类型。
+ *                        2、const: 针对主键或索引的等值查询扫描，最多只返回一行数据。 const 查询速度非常快， 因为它仅仅读取一次即可。
+ *                                 - 例如下面的这个查询，它使用了主键索引，因此 type 就是 const 类型的：explain select * from user_info where id = 2；
+ *                        3、eq_ref: 此类型通常出现在多表的 join 查询，表示对于前表的每一个结果，都只能匹配到后表的一行结果。并且查询的比较操作通常是 =，查询效率较高。
+ *                                 - 例如：explain select * from user_info, order_info where user_info.id = order_info.user_id;
+ *                        4、ref: 此类型通常出现在多表的 join 查询，针对于非或非主键索引，或者是使用了 最左前缀 规则索引的查询。
+ *                                 - 例如：explain select * from user_info, order_info where user_info.id = order_info.user_id AND order_info.user_id = 5
+ *                        5、range: 表示使用索引范围查询，通过索引字段范围获取表中部分数据记录。这个类型通常出现在 =, <>, >, >=, <, <=, IS NULL, <=>, BETWEEN, IN() 操作中。
+ *                        6、index: 表示全索引扫描(full index scan)，和 ALL 类型类似，只不过 ALL 类型是全表扫描，而 index 类型则仅仅扫描所有的索引， 而不扫描数据。
+ *                        7、ALL: 表示全表扫描，这个类型的查询是性能最差的查询之一。-  如一个查询是 ALL 类型查询， 那么一般来说可以对相应的字段添加索引来避免。
+ *          - 通常来说, 不同的 type 类型的性能关系如下: ALL < index < range  < ref < eq_ref < const < system
+ *      5、possible_keys ：它表示 mysql 在查询时，可能使用到的索引。
+ *      6、key ：此字段是 mysql 在当前查询时所真正使用到的索引。
+ *      7、key_len : 表示查询优化器使用了索引的 字节数，这个字段可以评估组合索引是否完全被使用。
+ *      8、ref ：这个表示显示索引的哪一列被使用了，如果可能的话,是一个常量。前文的type属性里也有ref，注意区别。
+ *      9、rows ：查询逻辑扫描过的 记录行数，这个值非常直观的显示 sql 效率好坏， 原则上 rows 越少越好。
+ *      10、extra ：额外信息，主要是指fetch data的具体方式。
+ *          - 常见的有以下几种内容: 1、using filesort:表示 mysql 需额外的排序操作,不能通过索引顺序达到排序效果。一般有using filesort都建议优化去掉,因为这样的查询 cpu 资源消耗大。
+ *                               2、using index：覆盖索引扫描，表示查询在索引树中就可查找所需数据，不用扫描表数据文件，往往说明性能不错。
+ *                               3、using temporary：查询有使用 临时表, 一般出现于排序， 分组和多表 join 的情况， 查询效率不高，建议优化。
+ *                               4、using where ：表名使用了where过滤。
+ *
  *《mysql常用命令大全》 - linux
  *  1、重启服务 - service mysqld restart
  *     查看状态 - service mysqld status
