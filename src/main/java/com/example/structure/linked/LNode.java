71c5c04676d83cfebcf60a885258ea7a1437dcfa
xiaoguozi <527768758@qq.com>
2018-08-20 20:52:15 +0800

    算法

---
+++ src/main/java/com/example/structure/linked/LNode.java
@@ -0,0 +1,11 @@
+package com.example.structure.linked;
+/**
+ * @author  xiaoguozi
+ * @create  2018/8/20 下午8:51
+ * @desc    单链表的结点类
+ **/
+public class LNode {
+    //为了简化访问单链表,结点中的数据项的访问权限都设为public
+    public int data;
+    public LNode next;
+}
--- src/main/java/com/example/structure/linked/LinkedLearn.java
+++ src/main/java/com/example/structure/linked/LinkedLearn.java
@@ -6,8 +6,27 @@ package com.example.structure.linked;
  * @desc    链表学习 -
  *
  * 1、判断一个单链表中是否有环？
+ *   - 思路：如果一个单链表中有环，用一个指针去遍历，永远不会结束，所以可以用两个指针，一个指针一次走一步，另一个指针一次走两步，
+ *          如果存在环，则这两个指针会在环内相遇，时间复杂度为O(n)。
  *
  **/
 public class LinkedLearn {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean hasCircle(LNode L)
+    {
+        LNode slow=L;//slow表示从头结点开始每次往后走一步的指针
+        LNode fast=L;//fast表示从头结点开始每次往后走两步的指针
+        while(fast!=null && fast.next!=null)
+        {
+            if(slow==fast) return true;//p与q相等，单链表有环
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return false;
+    }
 
 }
