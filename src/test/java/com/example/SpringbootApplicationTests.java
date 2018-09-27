ebf3bbb47d148b9ee8e9087f97ced3fb59212039
guoshaohua <guoshaohua@autohome.com.cn>
2018-09-27 09:22:02 +0800

    test

--- src/test/java/com/example/SpringbootApplicationTests.java
+++ src/test/java/com/example/SpringbootApplicationTests.java
@@ -29,10 +29,6 @@ public class SpringbootApplicationTests {
 	@Test
 	public void payTest() throws Exception {
 //		redisService.set(UUID.randomUUID().toString(),"test",1l, TimeUnit.HOURS);
-
-
-
-
 //		System.out.println("库存为：" + store);
 	}
 
