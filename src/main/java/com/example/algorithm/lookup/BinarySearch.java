fcb5a11d96e66ea3cb078fdcdd27f913f6d49782
xiaoguozi218 <527768758@qq.com>
2020-05-08 21:39:03 +0800

    算法题

--- src/main/java/com/example/algorithm/lookup/BinarySearch.java
+++ src/main/java/com/example/algorithm/lookup/BinarySearch.java
@@ -37,23 +37,23 @@ public class BinarySearch {
     }
 
     //递归实现二分查找
-    public static int binarySearch(int[] dataset,int data,int beginIndex,int endIndex){
-        int midIndex = (beginIndex+endIndex)/2;
-        if(data <dataset[beginIndex]||data>dataset[endIndex]||beginIndex>endIndex){
+    public static int binarySearch(int[] arr,int target,int left,int right){
+        int mid = (left + right)/2;
+        if(target < arr[left] || target > arr[right] || left > right){
             return -1;
         }
-        if(data <dataset[midIndex]){
-            return binarySearch(dataset,data,beginIndex,midIndex-1);
-        }else if(data>dataset[midIndex]){
-            return binarySearch(dataset,data,midIndex+1,endIndex);
-        }else {
-            return midIndex;
+        if (target < arr[mid]) {
+            return binarySearch(arr,target,left,mid-1);
+        } else if (target > arr[mid]){
+            return binarySearch(arr,target,mid+1,right);
+        } else {
+            return mid;
         }
     }
 
     public static void main(String[] args) {
         int[] arr = { 6, 12, 33, 87, 90, 97, 108, 561 };
-        System.out.println("循环查找：" + (binarySearch(arr, 87) + 1));
-        System.out.println("递归查找"+binarySearch(arr,87,3,arr.length-1));
+        System.out.println("循环查找:" + binarySearch(arr, 87));
+        System.out.println("递归查找:" + binarySearch(arr,87,3,arr.length-1));
     }
 }
