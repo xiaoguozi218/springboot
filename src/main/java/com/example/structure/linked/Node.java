151036977a89d4df8eb8bb6d9108c45faf904f8e
xiaoguozi218 <527768758@qq.com>
2020-06-02 20:32:02 +0800

    链表学习

--- src/main/java/com/example/structure/linked/Node.java
+++ src/main/java/com/example/structure/linked/Node.java
@@ -1,9 +1,13 @@
 package com.example.structure.linked;
+
+import lombok.Data;
+
 /**
  * @author  xiaoguozi
  * @create  2018/8/20 下午8:51
  * @desc    单链表的结点类
  **/
+@Data
 public class Node {
     protected Node next; //指针域
     protected int data; //数据域
