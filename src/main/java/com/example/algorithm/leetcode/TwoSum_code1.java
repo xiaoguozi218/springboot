8f97cc5795ec95ebdc78bea90f982cd734794b36
xiaoguozi218 <527768758@qq.com>
2020-06-29 21:00:05 +0800

    链表学习

--- src/main/java/com/example/algorithm/leetcode/TwoSum_code1.java
+++ src/main/java/com/example/algorithm/leetcode/TwoSum_code1.java
@@ -17,7 +17,7 @@ public class TwoSum_code1 {
      * 由于哈希查找的时间复杂度为 O(1)，所以可以利用哈希容器 map 降低时间复杂度
      * 遍历数组 nums，i 为当前下标，每个值都判断map中是否存在 target-nums[i] 的 key 值
      * 如果存在则找到了两个值，如果不存在则将当前的 (nums[i],i) 存入 map 中，继续遍历直到找到为止
-     * 如果最终都没有结果则抛出异常
+     * 如果最终都没有结果则抛出异常 或者 返回null
      * 时间复杂度：O(n)
      *
      * @author  gsh
@@ -34,7 +34,7 @@ public class TwoSum_code1 {
             }
             map.put(nums[i], i);
         }
-        throw new IllegalArgumentException("No two sum solution");
+        return null;
     }
 
 }
