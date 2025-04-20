77b4afc55319d33b860abca62f8c3d456832a844
gsh <527768758@qq.com>
2025-04-20 18:02:51 +0800

    mermaid文件

--- src/main/java/com/example/algorithm/LRUCache.java
+++ src/main/java/com/example/algorithm/LRUCache.java
@@ -51,5 +51,22 @@ public class LRUCache extends LinkedHashMap<Integer, Integer> {
     public boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest){
         return size()>capacity;
     }
+
+
+    public static void main(String[] args) {
+        LRUCache cache = new LRUCache(2);
+
+        cache.put(1, 1);
+        cache.put(2, 2);
+        System.out.println(cache.get(1)); // 返回 1
+
+        cache.put(3, 3); // 该操作会使得键 2 作废
+        System.out.println(cache.get(2)); // 返回 -1 (未找到)
+
+        cache.put(4, 4); // 该操作会使得键 1 作废
+        System.out.println(cache.get(1)); // 返回 -1 (未找到)
+        System.out.println(cache.get(3)); // 返回 3
+        System.out.println(cache.get(4)); // 返回 4
+    }
 }
 
